5e66d2ce1b97224d80b1b2a82eede2f07595748b
Sean Lee <lee.sean.cw@gmail.com>
2023-01-28 17:38:34 -0500

    remove unused useEffect import

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
